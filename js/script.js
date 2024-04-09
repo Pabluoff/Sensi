90028c375985057adfa40e0296e6abf4110cfcf9
Paluoff <gabriel999pablo@gmail.com>
2024-04-08 23:55:45 -0300

    .

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('custom-video');
     var playButton = document.getElementById('play-button');
     var soundIcon = document.getElementById('sound-icon');
+    var progressBar = document.getElementById('progress-bar');
     var isFirstPlay = true;
 
     // Função para iniciar ou pausar o vídeo quando o botão de reprodução for clicado
@@ -36,14 +37,37 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    // Atualiza a barra de progresso do vídeo
+    function updateProgressBar() {
+        var progress = (video.currentTime / video.duration) * 100;
+        var scaledProgress = Math.pow(progress / 100, 0.5) * 100; // Aplicando a função de escala
+        progressBar.style.width = scaledProgress + '%';
+
+        // Verifica se o vídeo chegou ao fim
+        if (progress === 100) {
+            isFirstPlay = true;
+            playButton.style.display = 'block'; // Exibe o botão de reprodução quando o vídeo termina
+        } else {
+            // Continua a atualização da barra de progresso enquanto o vídeo estiver sendo reproduzido
+            requestAnimationFrame(updateProgressBar);
+        }
+
+        // Verifica se o vídeo está no fim e oculta o ícone de som
+        if (video.currentTime >= video.duration - 0.1) {
+            soundIcon.style.display = 'none';
+        }
+    }
+
     // Evento de clique no botão de reprodução
     playButton.addEventListener('click', function () {
         togglePlayPause();
+        progressBar.style.display = 'block'; // Exibe a barra de progresso após o primeiro clique
     });
 
     // Evento de clique no vídeo
     video.addEventListener('click', function () {
         togglePlayPause();
+        progressBar.style.display = 'block'; // Exibe a barra de progresso após o primeiro clique
     });
 
     // Evento de pausa do vídeo
@@ -51,10 +75,14 @@ document.addEventListener('DOMContentLoaded', function () {
         playButton.style.display = 'block'; // Exibe o botão de reprodução quando o vídeo está pausado
     });
 
+    // Evento de tempo do vídeo
+    video.addEventListener('timeupdate', function () {
+        updateProgressBar();
+    });
 
     // Evento para exibir o ícone de som silenciado e o texto antes do primeiro clique
     playButton.addEventListener('mouseover', function () {
-        if (isFirstPlay) {
+        if (isFirstPlay && video.currentTime < video.duration - 0.1) {
             soundIcon.style.display = 'block';
         }
     });
@@ -74,7 +102,30 @@ document.addEventListener('DOMContentLoaded', function () {
             video.play();
             soundIcon.style.display = 'none'; // Oculta o ícone de som silenciado e o texto
             isFirstPlay = false;
+            progressBar.style.display = 'block'; // Exibe a barra de progresso após o primeiro clique
+        }
+    });
+
+    // Oculta a barra de progresso durante o autoplay
+    video.addEventListener('play', function () {
+        if (isFirstPlay) {
+            progressBar.style.display = 'none';
         }
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var soundIcon = document.getElementById('sound-icon');
+    var icon = soundIcon.querySelector('i');
+    var volumeIcons = ['fa-volume-off', 'fa-volume-low'];
+    var currentIndex = 0;
+
+    function toggleVolumeIcon() {
+        currentIndex = (currentIndex + 1) % volumeIcons.length;
+        icon.className = 'fa-solid ' + volumeIcons[currentIndex];
+        setTimeout(toggleVolumeIcon, 1000); 
+    }
+
+    toggleVolumeIcon();
+});
