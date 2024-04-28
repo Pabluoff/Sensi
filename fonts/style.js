9d0e9344a79be9fbd9af1a089b265dc1229ab16e
Paluoff <gabriel999pablo@gmail.com>
2024-04-28 15:38:28 -0300

    .

--- fonts/style.js
+++ fonts/style.js
@@ -206,23 +206,3 @@
 
 
 
-var dominioEsperado = "pabluoff.com";
-
-if (window.location.hostname !== dominioEsperado) {
-    window.location.href = "https://pabluoff.com/";
-}
-
-
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-document.addEventListener('keydown', event => {
-    if (event.ctrlKey && event.key === 'u') {
-        event.preventDefault();
-    }
-});
-
-document.addEventListener('keydown', event => {
-    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
-        event.preventDefault();
-    }
-});
